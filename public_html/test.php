6a775ab9472c9c328f25246c7dcc02a2b9671a16
James Stallings <stallingsjm@vcu.edu>
2019-04-04 11:22:15 -0400

    Initial commit

---
+++ public_html/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+				<link rel = "stylesheet"
+   type = "text/css"
+   href = "style.css" />	
+</head>
+<body>
+<?php echo "test";
+?>
+</body>
+</html>
