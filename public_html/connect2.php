f18a3ef0d30383892b6770655006fa4bd66ec16c
Jim Stallings <stallingsjm@mymail.vcu.edu>
2019-04-05 18:56:23 -0400

    Delete connect2.php

--- public_html/connect2.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn=oci_connect("V00859712","V00859712","128.172.188.107");
-    If (!$conn)
-        echo 'Failed to connect to Oracle';
-    else
-        echo 'Succesfully connected with Oracle DB';
- 
-oci_close($conn);
-?>
