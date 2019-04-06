95bbe62ab725b90ee7a909158915e68613ef86e6
Jim Stallings <stallingsjm@mymail.vcu.edu>
2019-04-06 17:05:07 -0400

    Add files via upload
    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict/EN" "http://www.w3.org/TR/xhtml1-strict.dtd">
+
+	<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+		<head>
+			<title>LoginSystem - Login System</title>
+				<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+			<link rel = "stylesheet"
+   type = "text/css"
+   href = "style.css" />	
+				
+		</head>
+	
+		<body>
+			<header><h1>Welcome to the FootRace management Application Web Iinterface</h1></header>
+  <div id="main">
+    <article><form action="process.php" method="POST">
+						<table align="center">
+<tr>
+								<td align="right">
+									<label class="">Username : </label>
+								</td>
+								<td>
+									<input type="text" id="u_email" name="u_email" class="text_box"> 
+								</td>
+								</tr>
+
+							<tr>
+								<td align="right">
+									<label class="label">Password : </label>
+								</td>
+								<td>
+									<input type="password" id="u_pass" name="u_pass" class="text_box"> 
+								</td>
+							</tr>
+							<tr>
+								<td align="center">
+									
+								</td>
+								<td style="padding-left:88px;">
+									 <input type="submit" name="action" value="Sign In" title="Sign In" class="submit"/>
+								</td>
+							</tr>
+</table>
+					</form>
+		<h2><span style="color:red">Does not</span> actually DO anything yet!</h2>
+	  
+	  </article>
+    <nav>Nav</nav>
+    <aside><a title="See page for author [Public domain], via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Maksymilian_Hartlik.jpg"><img width="256" alt="Maksymilian Hartlik" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Maksymilian_Hartlik.jpg/256px-Maksymilian_Hartlik.jpg"></a></aside>
+  </div>
+  <footer>James Stallings and Daniel McNamara For CMSC 508 Spring 2019</footer>
+</body>
+	</html>
